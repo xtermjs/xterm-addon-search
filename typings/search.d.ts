8e473df2558b947ffb34e406e1bd0d068c49a132
Daniel Imms <daimms@microsoft.com>
2019-05-12 10:05:12 -0700

    Initial implementation

--- typings/search.d.ts
+++ typings/search.d.ts
@@ -2,3 +2,28 @@
  * Copyright (c) 2017 The xterm.js authors. All rights reserved.
  * @license MIT
  */
+
+import { Terminal, ILinkMatcherOptions, IDisposable } from 'xterm';
+
+// TODO: This is temporary, link to xterm when the new version is published
+export interface ITerminalAddon extends IDisposable {
+  activate(terminal: Terminal): void;
+}
+
+export interface ISearchOptions {
+  regex?: boolean;
+  wholeWord?: boolean;
+  caseSensitive?: boolean;
+  /**
+   * Use this when you want the selection to expand if it still matches as the
+   * user types. Note that this only affects findNext.
+   */
+  incremental?: boolean;
+}
+
+export class SearchAddon implements ITerminalAddon {
+  public activate(terminal: Terminal): void;
+  public dispose(): void;
+  public findNext(term: string, searchOptions?: ISearchOptions): boolean;
+  public findPrevious(term: string, searchOptions?: ISearchOptions): boolean;
+}
